743540725c90c87eabada342e202d86f928c21c9
Francisco <francisco_e3@hotmail.com>
2021-07-15 02:49:44 -0400

    actualización en pagina principal, tamaños de imagenes responsivas y ajustes en anchos de slides para lograr mismo efecto que maqueta donde los botones del slide sobresalen

--- js/app.js
+++ js/app.js
@@ -1,24 +1,102 @@
 $(document).ready(function () {
 
-    $('.slide-individual').owlCarousel({
+    $('.owl-header').owlCarousel({
         items: 1,
         singleItem: true,
         loop:true,
         margin: 0,
         nav:true,
+        pagination: true,
+        autoplay: true,
+        resposiveClass: true,
+        responsive:{
+            0:{
+                items:1,
+                nav: false,
+                pagination: false,
+            },
+            600:{
+                items:1,
+                nav: false,
+                pagination: false,
+            },
+            1000:{
+                items:1,
+            }
+        }
+    })
+
+    $('.owl-productos').owlCarousel({
+        items: 1,
+        singleItem: true,
+        loop:true,
+        margin: 0,
+        nav:false,
         pagination: false,
-        autoplay: true
+        autoplay: true,
+        resposiveClass: true,
+        responsive:{
+            0:{
+                items:1,
+            },
+            600:{
+                items:1,
+            },
+            1000:{
+                items:4,
+                nav: true,
+                pagination: true,
+            }
+        }
     });
 
     $('.owl-stagePadding').owlCarousel({
         items: 1,
+        margin: 15,
         singleItem: true,
-        loop: true,
-        margin: 100,
-        stagePadding: 200,
+        loop: false,
         nav: true,
         pagination: true,
-        autoplay: true
+        autoplay: true,
+        resposiveClass: true,
+        responsive:{
+            0:{
+                items:1,
+                margin: 100,
+                stagePadding: 200,
+                loop: true,
+            },
+            600:{
+                items:1,
+                margin: 100,
+                stagePadding: 200,
+                loop: true,
+            },
+            1000:{
+                items:5,
+            }
+        }
+    });
+
+    $('.owl-cat').owlCarousel({
+        singleItem: true,
+        loop: true,
+        margin: 20,
+        nav: true,
+        pagination: true,
+        autoplay: true,
+        resposiveClass: true,
+        responsive:{
+            0:{
+                items:1,
+            },
+            600:{
+                items:1,
+            },
+            1000:{
+                items:2,
+            }
+        }
     })
 
 });
